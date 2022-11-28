c2b1e1fc51f03efb993f7c44dea83b8a737b1a6e
AM-ASKY <am.asky97@gmail.com>
2022-11-28 10:49:49 +0530

    changw

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World welcome
+          Hello Welcome my page
         </p>
         <a
           className="App-link"
